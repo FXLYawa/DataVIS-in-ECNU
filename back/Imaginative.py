71ff762b6b27ef7d9ee5b16af2cb9c6aeb766119
FXLYawa <10235102444@stu.ecnu.edu.cn>
2025-06-07 20:12:44 +0800

    imaginative update

--- back/Imaginative.py
+++ back/Imaginative.py
@@ -12,6 +12,12 @@ def get_api_key(file_path: str) -> str:
         raise FileNotFoundError(f"API 密钥文件未找到: {file_path}")
     with open(file_path, 'r') as f:
         return f.read().strip()
+    
+def get_prompt(file_path: str) -> str:
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"歌词文件未找到: {file_path}")
+    with open(file_path, 'r') as f:
+        return f.read().strip()
 
 def ask(s:str, api_key: str):
     rsp = ImageSynthesis.call(api_key=api_key,
@@ -29,9 +35,25 @@ def ask(s:str, api_key: str):
             (rsp.status_code, rsp.code, rsp.message))
     return res
 
+
+def imaginative(name):
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    api_key_file = os.path.join(file_dir, 'data', 'apikey.txt')  
+    api_key = get_api_key(api_key_file)
+    
+    prompt_file = os.path.join(file_dir, 'data', name + '.txt')
+    prompt = get_prompt(prompt_file)
+    arr = ask(prompt)
+    for x in arr:
+        file_name = PurePosixPath(unquote(urlparse(x).path)).parts[-1]
+        with open('./{}'.format(file_name), 'wb+') as f:
+            f.write(requests.get(x).content)
+    return arr
+
+
 if __name__ == "__main__":
     file_dir = os.path.dirname(os.path.abspath(__file__))
-    api_key_file = os.path.join(file_dir, 'apikey.txt')  
+    api_key_file = os.path.join(file_dir, 'data', 'apikey.txt')  
     api_key = get_api_key(api_key_file)
     
     prompt = input()
---
+++ back/README.md
@@ -0,0 +1,6 @@
+### 意象图
+
+imaginative中的imaginative函数
+
+- **input**：歌名
+- **output**：意象图url
